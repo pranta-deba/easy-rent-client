798711e342df2c8f2a5241b54c8811c068d67fcb
Pranto Deb <rajprit0m@outlook.com>
2024-07-15 13:52:11 +0600

    [Created New File] : create a layoutpage, error page, navbar & footer component file, and banner component file

---
+++ src/components/Shared/Error.jsx
@@ -0,0 +1,9 @@
+const Error = () => {
+    return (
+        <div>
+            error page
+        </div>
+    );
+};
+
+export default Error;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Root/Layout";
+import Error from "../components/Shared/Error";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Layout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+        ],
     },
 ]);
