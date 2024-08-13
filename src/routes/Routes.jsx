aad49467512b1e7afaf20ba3a5caa0061960160c
git12nazneen <lipinazneen65@gmail.com>
2024-08-14 00:24:10 +0600

    login ref

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,8 @@ import Root from "../Layout/Root";
 import Error from "../pages/Error/Error";
 import Home from "../pages/Error/Home/Home";
 import Blog from "../components/Blog/Blog";
+import Login from "../components/SignInSignUp/Login";
+import Register from "../components/SignInSignUp/Register";
 
 export const router = createBrowserRouter([
     {
@@ -19,7 +21,15 @@ export const router = createBrowserRouter([
             {
                 path:'/blog',
                 element:<Blog/>,
-            }
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            },
+            {
+                path:"/register",
+                element:<Register></Register>
+            },
         ],
     },
 ]);
